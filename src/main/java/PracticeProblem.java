ce348a4c749baa9c2f01fcd765c7fce54db165c5
justin-chu-ics4u-03-25 <justin.chu4@student.tdsb.on.ca>
2025-02-24 05:31:15 +0000

    My answers

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,4 +4,57 @@ public class PracticeProblem {
 
 	}
 
+	public static String pluralize(String word) {
+		String lowercase = word.toLowerCase();
+
+		if (lowercase.endsWith("ife")) {
+			return "ives";
+		}
+
+		else if (lowercase.endsWith("ey")) {
+			return "eys";
+		}
+
+		else if (lowercase.endsWith("y")) {
+			return "ies";	
+		}
+
+		else {
+			return "s";
+		}
+	}
+
+	public static int min(int a, int b, int c) {
+		int smalboi = a;
+
+		if (b < smalboi) {
+			smalboi = b;
+		}
+
+		if (c < smalboi) {
+			smalboi = c;
+		}
+
+		return smalboi;
+	}
+
+	public static boolean isLeapYear(int year) {
+		if (year % 4 == 0) {
+			if (year % 100 == 0) {
+				if (year % 400 ==0) {
+					return true;
+				}
+				else {
+					return false;
+				}
+			}
+			else {
+				return true;
+			}
+		}
+		else {
+			return false;
+		}
+	}
+
 }
